13a18d10c4dc163c4910cae7aaa8989acde0bccc
Cauã Augusto <162489346+cauaaugustow@users.noreply.github.com>
2026-05-11 15:22:13 -0300

    add- Ultimos ajustes, e fim do desafio

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,49 @@ function limpaInput(){
     inputTarefa.focus();
 }
 
+function criarBotaoApagar(li){
+    const botaoApagar = document.createElement('button')
+    botaoApagar.innerText = 'Apagar'
+    li.appendChild(botaoApagar)
+    botaoApagar.setAttribute('class','botao-apagar')
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa')
+}
+
+
+document.addEventListener('click', function(e){
+    const el = e.target;
+
+    if(el.classList.contains('botao-apagar')){
+        el.parentElement.remove()
+    }
+
+})
+
+function salvarTarefas(){
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for (let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '');
+        listaDeTarefas.push(tarefaTexto)
+        console.log(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON)
+}
+
+function adicionaTarefasSalvas(){
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+
+    for (let tarefa of listaDeTarefas){
+        criaTarefa(tarefa);
+    }
+}
+adicionaTarefasSalvas();
+
 function criarLista(){
     const lista = document.createElement('li')
     return lista;
@@ -24,7 +67,9 @@ function criaTarefa(textoInput){
     const li = criarLista();
     li.innerText = textoInput;
     tarefas.appendChild(li);
+    criarBotaoApagar(li);
     limpaInput();
+    salvarTarefas();
 }
 
 btnTarefa.addEventListener('click', function(e){
